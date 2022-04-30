bee435e2f43f6021353bbcd4185a26592fd64c9b
PatchWorkCoding <85509178+PatchWorkCoding@users.noreply.github.com>
2022-04-30 03:10:10 -0500

    Improve Lure Behavior
    Added Rotation and movement outside of combat to the lure

--- ScoreJam18/Assets/LureBehavior.cs
+++ ScoreJam18/Assets/LureBehavior.cs
@@ -1,38 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class LureBehavior : MonoBehaviour
 {
+    [Header("Outside Combat Properties")]
     [SerializeField]
-    AnimationCurve lureDeccelerationCurve;
+    float rotationSpeed = 1;
     [SerializeField]
-    float fallTimeScale = 0.5f;
-
+    bool doClamp = false;
+    [SerializeField]
+    Vector2 rotationMinMax = Vector2.zero;
+    [SerializeField]
+    float moveSpeed = 1;
+    [SerializeField]
+    float gravity = 0.5f;
+    
     Rigidbody RB = null;
-    bool isFalling = false;
-
-    float fallTime = 0;
-    float speed = 0;
-
+    float rotZ = 0;
+    float previousLoggedTime = 0;
+    bool movingForward = false;
     PlayerBehavior myPlayer = null;
+    Vector2 input = Vector2.zero;
+
+    private void Start()
+    {
+        Init();
+    }
 
     // Start is called before the first frame update
-    public void Init(float _speed, Vector3 _dir, PlayerBehavior _player)
+    public void Init()
     {
+        RB = GetComponent<Rigidbody>();
+        /*
         fallTime = 1;
         speed = _speed;
         myPlayer = _player;
         isFalling = true;
 
-        RB = GetComponent<Rigidbody>();
+        
         RB.velocity = _dir * _speed;
+        */
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (doClamp)
+        {
+            rotZ = Mathf.Clamp(rotZ + (input.x * rotationSpeed * Time.deltaTime), rotationMinMax.x, rotationMinMax.y);
+            
+        }
+        else
+        {
+            rotZ += (input.x * rotationSpeed * Time.deltaTime);
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        RB.velocity = (Vector3.down * gravity) + (transform.up * -(movingForward ? moveSpeed : 0));
+
+        /*
         if (isFalling && fallTime > 0)
         {
             RB.velocity = RB.velocity.normalized * (lureDeccelerationCurve.Evaluate(fallTime) * speed);
@@ -44,15 +74,38 @@ public class LureBehavior : MonoBehaviour
                 RB.velocity = Vector3.zero;
             }
         }
+        */
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void RotateDrone(InputAction.CallbackContext _ctx)
     {
-        if (collision.gameObject.tag == "fish")
+        input = _ctx.ReadValue<Vector2>();
+        
+        /*
+        if (_ctx.performed)
         {
-            myPlayer.CaughtFish(collision.gameObject.GetComponent<FishBehavior>());
-            collision.gameObject.GetComponent<FishBehavior>().ChangeFishState(FishState.COMBAT);
-            Destroy(gameObject);
+            
+            //previousLoggedTime = Time.time;
         }
+        */
+    }
+
+    public void MoveFoward(InputAction.CallbackContext _ctx) 
+    {
+        if (_ctx.started)
+        {
+            movingForward = true;
+        }
+
+        else if (_ctx.canceled)
+        {
+            movingForward = false;
+        }
+    }
+
+
+    public void StickToObject() 
+    { 
+        
     }
 }
